36b9d12c60a955d3ce1bea09ecb9c94e66659962
brunomacedo <contato@brunomacedo.com.br>
2022-02-07 16:25:04 -0300

    chore: up list size

--- src/services/menu.services.tsx
+++ src/services/menu.services.tsx
@@ -1,31 +1,457 @@
 import React from 'react';
+import uuid from 'react-native-uuid';
 import {ChatText, ShoppingBagOpen, Tag, UserList} from 'phosphor-react-native';
 import {IconStore} from '../components/Icons/store';
 import styles from '../theme/styles';
 
-export const MenuItems = [
+export const MenuItems = () => [
   {
-    id: '1',
+    id: `${uuid.v4()}`,
     icon: <IconStore size={50} color={styles.primary.color} />,
     text: 'Loja',
   },
   {
-    id: '2',
+    id: `${uuid.v4()}`,
     icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
     text: 'Ofertas',
   },
   {
-    id: '3',
+    id: `${uuid.v4()}`,
     icon: <Tag size={50} color={styles.primary.color} />,
     text: 'Cupons',
   },
   {
-    id: '4',
+    id: `${uuid.v4()}`,
     icon: <ChatText size={50} color={styles.primary.color} />,
     text: 'Ajuda',
   },
   {
-    id: '5',
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <UserList size={50} color={styles.primary.color} />,
+    text: 'Conta',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <IconStore size={50} color={styles.primary.color} />,
+    text: 'Loja',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ShoppingBagOpen size={50} color={styles.primary.color} />,
+    text: 'Ofertas',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <Tag size={50} color={styles.primary.color} />,
+    text: 'Cupons',
+  },
+  {
+    id: `${uuid.v4()}`,
+    icon: <ChatText size={50} color={styles.primary.color} />,
+    text: 'Ajuda',
+  },
+  {
+    id: `${uuid.v4()}`,
     icon: <UserList size={50} color={styles.primary.color} />,
     text: 'Conta',
   },
